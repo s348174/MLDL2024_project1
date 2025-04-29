b9cef06204c275bb123e9aec8127d1222ff016c1
alberto <prinoalberto02@gmail.com>
2025-04-29 12:27:50 +0200

    Modified .gitignore in order not to trace python environment and dataset folder. Removed dataset

--- datasets/cityscapes.py
+++ datasets/cityscapes.py
@@ -1,21 +1,32 @@
 from torch.utils.data import Dataset
-
-# TODO: implement here your custom dataset class for Cityscapes
-
+from PIL import Image
+import os
 
 class CityScapes(Dataset):
-    def __init__(self):
+    def __init__(self, root_dir, transform=None):
         super(CityScapes, self).__init__()
-        # TODO
+        self.root_dir = root_dir
+        self.transform = transform
+        self.samples = []
+        self.class_to_idx = {}
+        self.classes = []
 
-        pass
+        # Scan subfolders for classes and images
+        for class_name in sorted(os.listdir(root_dir)):
+            class_path = os.path.join(root_dir, class_name)
+            if os.path.isdir(class_path):
+                self.classes.append(class_name)
+                self.class_to_idx[class_name] = len(self.class_to_idx)
+                for fname in sorted(os.listdir(class_path)):
+                    if fname.lower().endswith(('.png', '.jpg', '.jpeg')):
+                        self.samples.append((os.path.join(class_path, fname), self.class_to_idx[class_name]))
 
     def __getitem__(self, idx):
-        # TODO
-
-        pass
+        img_path, label = self.samples[idx]
+        image = Image.open(img_path).convert("RGB")
+        if self.transform:
+            image = self.transform(image)
+        return image, label
 
     def __len__(self):
-        # TODO
-
-        pass
+        return len(self.samples)
--- train.py
+++ train.py
@@ -1,1 +1,33 @@
 # TODO: Define here your training and validation loops.
+
+from torchvision import transforms
+from datasets.read_cityscapes import CityScapes
+# Setup training data
+# Assuming the dataset is structured as follows:
+# /path/to/train/class1/image1.png
+# /path/to/train/class1/image2.png
+# /path/to/train/class2/image1.png
+# /path/to/train/class2/image2.png
+
+
+dataset_path = "/home/alberto/Documenti/Materiale scuola Alberto/MLDL2024_project1/datasets/Cityscapes/Cityspaces/images"
+train_path = dataset_path + "/train"
+dataset = CityScapes(train_path, transform=transforms.ToTensor())
+class_names = dataset.classes
+print(f"Class names: {class_names}")
+print(f"Number of classes: {len(class_names)}")
+print(f"Number of training samples: {len(dataset)}")
+print(f"Number of test samples: {len(test_data)}")
+print(f"Image shape: {dataset[0][0].shape}")
+# Let's visualize the first training sample
+image, label = dataset[0]
+plt.imshow(image.permute(1, 2, 0))
+plt.title(f"Label: {class_names[label]}")
+plt.axis("off")
+plt.show()
+# Let's visualize the first test sample
+image, label = test_data[0]
+plt.imshow(image.permute(1, 2, 0))
+plt.title(f"Label: {class_names[label]}")
+plt.axis("off")
+plt.show()
