0a46164e83c3399a4cc1b8a8be2ba4c950748afc
alberto <prinoalberto02@gmail.com>
2025-05-11 21:40:37 +0200

    Modified the local launch script to use the new training script.

--- local_launch.py
+++ local_launch.py
@@ -1,4 +1,5 @@
 from train import deeplab_train
 
 dataset_path_alberto = "/home/alberto/Documenti/Materiale scuola Alberto/MLDL2024_project1/datasets/Cityscapes/Cityspaces"
-deeplab_train(dataset_path_alberto)
+pretrain_path = "/home/alberto/Documenti/Materiale scuola Alberto/MLDL2024_project1/deepla_resnet_pretrained_imagenet.pth"
+deeplab_train(dataset_path_alberto, pretrain_path)
