55cdc4c6bf0e8d62e325d199ee8d8831b1115e44
alberto <prinoalberto02@gmail.com>
2025-05-11 21:43:40 +0200

    Solved path issues

--- local_launch.py
+++ local_launch.py
@@ -1,5 +1,5 @@
 from train import deeplab_train
 
 dataset_path_alberto = "/home/alberto/Documenti/Materiale scuola Alberto/MLDL2024_project1/datasets/Cityscapes/Cityspaces"
-pretrain_path = "/home/alberto/Documenti/Materiale scuola Alberto/MLDL2024_project1/deepla_resnet_pretrained_imagenet.pth"
+pretrain_path = "/home/alberto/Documenti/Materiale scuola Alberto/MLDL2024_project1/deeplab_resnet_pretrained_imagenet.pth"
 deeplab_train(dataset_path_alberto, pretrain_path)
