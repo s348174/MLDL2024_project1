c7ed926c54c0277704ba1eba8537044849751df5
alberto <prinoalberto02@gmail.com>
2025-05-18 20:04:54 +0200

    Synchronize for final testing

--- local_launch.py
+++ local_launch.py
@@ -7,12 +7,14 @@ workspace_path = "/home/alberto/Documenti/Materiale scuola Alberto/MLDL2024_proj
 pretrained_image_path = "/home/alberto/Documenti/Materiale scuola Alberto/MLDL2024_project1/export/deeplabv2_epoch_22.pth"
 num_epochs = 1
 #deeplab_train(dataset_path_alberto, workspace_path, pretrained_image_path, num_epochs)
-model_path = workspace_path + "/export/deeplabv2_final.pth"
-#deeplab_test(dataset_path_alberto, model_path)
+model_path = workspace_path + "/export/deeplabv2_epoch_8.pth"
+deeplab_test(dataset_path_alberto, model_path)
 
+"""
 class_frequencies = compute_class_weights(dataset_path_alberto + "/gtFine/val", num_classes=19)
 for class_idx, freq in class_frequencies.items():
     print(f"Metric {class_idx}:")
     for i in range(len(freq)):
         print(f"Class {i}: {freq[i]}; ")
     print("\n")
+    """
