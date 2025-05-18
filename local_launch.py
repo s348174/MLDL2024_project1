e425edd4526dfab55655aa5dde4ef3fa53e8a896
alberto <prinoalberto02@gmail.com>
2025-05-18 22:38:20 +0200

    Push local_launch changes

--- local_launch.py
+++ local_launch.py
@@ -6,9 +6,9 @@ dataset_path_alberto = "/home/alberto/Documenti/Materiale scuola Alberto/MLDL202
 workspace_path = "/home/alberto/Documenti/Materiale scuola Alberto/MLDL2024_project1"
 pretrained_image_path = "/home/alberto/Documenti/Materiale scuola Alberto/MLDL2024_project1/deeplab_resnet_pretrained_imagenet.pth"
 num_epochs = 1
-#deeplab_train(dataset_path_alberto, workspace_path, pretrained_image_path, num_epochs)
+deeplab_train(dataset_path_alberto, workspace_path, pretrained_image_path, num_epochs)
 model_path = workspace_path + "/export/deeplabv2_epoch_8.pth"
-#deeplab_test(dataset_path_alberto, model_path)
+deeplab_test(dataset_path_alberto, model_path)
 
 """
 class_frequencies = compute_class_weights(dataset_path_alberto + "/gtFine/val", num_classes=19)
