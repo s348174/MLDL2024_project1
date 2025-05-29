4aab1ccc760acbf872cb4fb2035fce0c1627c2b9
Emanuele Martin <162364474+EmanueleMartin2001@users.noreply.github.com>
2025-05-29 13:21:36 +0200

    Update train.py
    batch size set to 4

--- train.py
+++ train.py
@@ -26,7 +26,7 @@ from torch.amp import autocast, GradScaler
 # TRAINIG DEEPLAB
 #################
 
-def deeplab_train(dataset_path, workspace_path, pretrain_imagenet_path, num_epochs=50, batch_size=2): 
+def deeplab_train(dataset_path, workspace_path, pretrain_imagenet_path, num_epochs=50, batch_size=4): 
     # Set the environment variable for PyTorch CUDA memory allocation
     os.environ['PYTORCH_CUDA_ALLOC_CONF'] = 'expandable_segments:True'
 
