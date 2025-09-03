5a8f1f8c3b099cfaaaf905490a0c6c0dea6e8094
alberto <prinoalberto02@gmail.com>
2025-09-03 11:10:12 +0200

    Modified savings on train.py to include augmentation, adaptive_epoch, and stopping_epoch in the saved model file.

--- local_launch.py
+++ local_launch.py
@@ -3,22 +3,22 @@ from utils import compute_class_weights, convert_weights_format
 import os
 import torch
 
-#dataset_path_alberto = "/home/alberto/Documenti/Materiale scuola Alberto/MLDL2024_project1/datasets/Cityscapes/Cityspaces"
-dataset_path_emanuele = "C:/Users/marti/OneDrive/Desktop/HW_Masone/MLDL2024_project1/datasets/Cityscapes/Cityspaces"
-#workspace_path = "/home/alberto/Documenti/Materiale scuola Alberto/MLDL2024_project1"
+dataset_path_alberto = "/home/alberto/Documenti/Materiale scuola Alberto/MLDL2024_project1/datasets/Cityscapes/Cityspaces"
+#dataset_path_emanuele = "C:/Users/marti/OneDrive/Desktop/HW_Masone/MLDL2024_project1/datasets/Cityscapes/Cityspaces"
+workspace_path = "/home/alberto/Documenti/Materiale scuola Alberto/MLDL2024_project1"
 #pretrained_image_path_alberto = "/home/alberto/Documenti/Materiale scuola Alberto/MLDL2024_project1/deeplab_resnet_pretrained_imagenet.pth"
 num_epochs = 1
 #deeplab_train(dataset_path_alberto, workspace_path, pretrained_image_path, num_epochs)
 
-#model_path_alberto = workspace_path + "/export/bisenet_adversarial_50_epoch_balanced_max_highres_2_batches.pth"
-model_path_emanuele = "C:/Users/marti/OneDrive/Desktop/HW_Masone/MLDL2024_project1/bisenet_on_gta_final_00001_new.pth"
+model_path_alberto = workspace_path + "/export/bisenet_adversarial_epoch_42_10111.pth"
+#model_path_emanuele = "C:/Users/marti/OneDrive/Desktop/HW_Masone/MLDL2024_project1/bisenet_on_gta_final_00001_new.pth"
     #augmentation = "wxyza" w=rotate, x=multiply, y=blur, z=flip, a=color_jitter (1 yes, 0 no)
 
 
 #deeplab_test(dataset_path_emanuele, model_path)
 #convert_weights_format(model_path, 50, 2, False, context_path='resnet18')
-#bisenet_test(dataset_path_alberto, model_path_alberto)
-bisenet_test(dataset_path_emanuele, model_path_emanuele)
+bisenet_test(dataset_path_alberto, model_path_alberto)
+#bisenet_test(dataset_path_emanuele, model_path_emanuele)
 
 #gta_path = "/home/alberto/Documenti/Materiale scuola Alberto/MLDL2024_project1/datasets/GTA5"
 #gta_model = "/home/alberto/Documenti/Materiale scuola Alberto/MLDL2024_project1/export/bisenet_on_gta_final_4_batches_balanced_polylr.pth"
